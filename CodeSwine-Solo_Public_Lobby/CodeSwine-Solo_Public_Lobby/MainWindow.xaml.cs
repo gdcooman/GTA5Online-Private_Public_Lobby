36113159d2dac8ee2d252e8de9fe48fe85c41381
Glenn De Cooman <glenn.decooman@hotmail.be>
2020-06-12 02:48:20 +0200

    Add button to block internet

--- CodeSwine-Solo_Public_Lobby/CodeSwine-Solo_Public_Lobby/MainWindow.xaml.cs
+++ CodeSwine-Solo_Public_Lobby/CodeSwine-Solo_Public_Lobby/MainWindow.xaml.cs
@@ -6,66 +6,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Interop;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace CodeSwine_Solo_Public_Lobby
-{
-    public partial class MainWindow : Window
-    {
+namespace CodeSwine_Solo_Public_Lobby {
+    public partial class MainWindow : Window {
         private IPTool iPTool = new IPTool();
         private DaWhitelist whiteList = new DaWhitelist();
         private List<IPAddress> addresses = new List<IPAddress>();
         private MWhitelist mWhitelist = new MWhitelist();
 
-        private bool set = false;
-        private bool active = false;
+        private bool lobbyRulesSet = false;
+        private bool lobbyRulesActive = false;
+        private bool internetRuleSet = false;
+        private bool internetRuleActive = false;
 
-        public MainWindow()
-        {
+        public MainWindow() {
             InitializeComponent();
             Loaded += MainWindow_Loaded;
         }
 
-        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
-        {
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e) {
             FirewallRule.lblAdmin = lblAdmin;
             Init();
         }
 
-        void Init()
-        {
+        void Init() {
             lblYourIPAddress.Content += " " + iPTool.IpAddress + ".";
             addresses = DaWhitelist.ReadIPsFromJSON();
             lsbAddresses.ItemsSource = addresses;
-            foreach (IPAddress ip in addresses)
-            {
+            foreach (IPAddress ip in addresses) {
                 mWhitelist.Ips.Add(ip.ToString());
             }
             SetIpCount();
         }
 
-        private void btnAdd_Click(object sender, RoutedEventArgs e)
-        {
-            if(IPTool.ValidateIP(txbIpToAdd.Text))
-            {
-                if(!addresses.Contains(IPAddress.Parse(txbIpToAdd.Text)))
-                {
+        private void btnAdd_Click(object sender, RoutedEventArgs e) {
+            if (IPTool.ValidateIP(txbIpToAdd.Text)) {
+                if (!addresses.Contains(IPAddress.Parse(txbIpToAdd.Text))) {
                     addresses.Add(IPAddress.Parse(txbIpToAdd.Text));
                     lsbAddresses.Items.Refresh();
                     mWhitelist.Ips.Add(txbIpToAdd.Text);
                     DaWhitelist.SaveToJson(mWhitelist);
-                    set = false; active = false;
+                    lobbyRulesSet = false; lobbyRulesActive = false;
+                    internetRuleSet = false; internetRuleActive = false;
                     FirewallRule.DeleteRules();
                     SetIpCount();
                     UpdateNotActive();
@@ -73,82 +58,110 @@ namespace CodeSwine_Solo_Public_Lobby
             }
         }
 
-        private void btnDelete_Click(object sender, RoutedEventArgs e)
-        {
-            if(lsbAddresses.SelectedIndex != -1)
-            {
+        private void btnDelete_Click(object sender, RoutedEventArgs e) {
+            if (lsbAddresses.SelectedIndex != -1) {
                 mWhitelist.Ips.Remove(lsbAddresses.SelectedItem.ToString());
                 addresses.Remove(IPAddress.Parse(lsbAddresses.SelectedItem.ToString()));
                 lsbAddresses.Items.Refresh();
                 DaWhitelist.SaveToJson(mWhitelist);
-                set = false; active = false;
+                lobbyRulesSet = false; lobbyRulesActive = false;
+                internetRuleSet = false; internetRuleActive = false;
                 FirewallRule.DeleteRules();
                 SetIpCount();
                 UpdateNotActive();
             }
         }
 
-        private void SetIpCount()
-        {
+        private void SetIpCount() {
             lblAmountIPs.Content = addresses.Count() + " IPs whitelisted!";
         }
 
-        private void btnEnableDisable_Click(object sender, RoutedEventArgs e)
-        {
+        private void btnEnableDisable_Click(object sender, RoutedEventArgs e) {
             SetRules();
         }
 
-        void SetRules()
-        {
+        private void btnEnableDisableInternet_Click(object sender, RoutedEventArgs e) {
+            ToggleInternet();
+        }
+
+        void SetRules() {
             string remoteAddresses = RangeCalculator.GetRemoteAddresses(addresses);
 
             // If the firewall rules aren't set yet.
-            if (!set)
-            {
+            if (!lobbyRulesSet) {
                 FirewallRule.CreateInbound(remoteAddresses, true, false);
                 FirewallRule.CreateOutbound(remoteAddresses, true, false);
-                active = true;
-                set = true;
+                lobbyRulesActive = true;
+                lobbyRulesSet = true;
                 UpdateActive();
                 return;
             }
 
             // If they are set but not enabled.
-            if (set && !active)
-            {
+            if (lobbyRulesSet && !lobbyRulesActive) {
                 FirewallRule.CreateInbound(remoteAddresses, true, true);
                 FirewallRule.CreateOutbound(remoteAddresses, true, true);
-                active = true;
+                lobbyRulesActive = true;
                 UpdateActive();
                 return;
             }
 
             // If they are active and set.
-            if(active && set)
-            {
+            if (lobbyRulesActive && lobbyRulesSet) {
                 FirewallRule.CreateInbound(remoteAddresses, false, true);
                 FirewallRule.CreateOutbound(remoteAddresses, false, true);
                 UpdateNotActive();
-                active = false;
+                lobbyRulesActive = false;
             }
         }
 
-        void UpdateNotActive()
-        {
+        void ToggleInternet() {
+            if (!internetRuleSet) {
+                FirewallRule.CreateInternetBlockRule(true, false);
+                internetRuleActive = true;
+                internetRuleSet = true;
+                ToggleInternetBtnColor();
+            }
+            else {
+                if (!internetRuleActive) {
+                    FirewallRule.CreateInternetBlockRule(true, true);
+                    internetRuleActive = true;
+                    ToggleInternetBtnColor();
+                }
+                else {
+                    FirewallRule.CreateInternetBlockRule(false, true);
+                    internetRuleActive = false;
+                    ToggleInternetBtnColor();
+                }
+            }
+
+        }
+
+        void UpdateNotActive() {
             btnEnableDisable.Background = ColorBrush.Red;
             image4.Source = new BitmapImage(new Uri("/CodeSwine-Solo_Public_Lobby;component/ImageResources/unlocked.png", UriKind.Relative));
             lblLock.Content = "Rules not active." + Environment.NewLine + "Click to activate!";
         }
 
-        void UpdateActive()
-        {
+        void UpdateActive() {
             btnEnableDisable.Background = ColorBrush.Green;
             image4.Source = new BitmapImage(new Uri("/CodeSwine-Solo_Public_Lobby;component/ImageResources/locked.png", UriKind.Relative));
             lblLock.Content = "Rules active." + Environment.NewLine + "Click to deactivate!";
         }
 
+        void ToggleInternetBtnColor() {
+            if (internetRuleActive) {
+                btnEnableDisableInternet.Background = ColorBrush.Red;
+                lblInternet.Content = "Internet disabled" + Environment.NewLine + "Click to activate!";
+            }
+            else {
+                btnEnableDisableInternet.Background = ColorBrush.Green;
+                lblInternet.Content = "Internet enabled" + Environment.NewLine + "Click to deactivate!";
+            }
+        }
+
         [DllImport("User32.dll")]
-            private static extern bool RegisterHotKey(
+        private static extern bool RegisterHotKey(
         [In] IntPtr hWnd,
         [In] int id,
         [In] uint fsModifiers,
@@ -162,8 +175,7 @@ namespace CodeSwine_Solo_Public_Lobby
         private HwndSource _source;
         private const int HOTKEY_ID = 9000;
 
-        protected override void OnSourceInitialized(EventArgs e)
-        {
+        protected override void OnSourceInitialized(EventArgs e) {
             base.OnSourceInitialized(e);
             var helper = new WindowInteropHelper(this);
             _source = HwndSource.FromHwnd(helper.Handle);
@@ -171,8 +183,7 @@ namespace CodeSwine_Solo_Public_Lobby
             RegisterHotKey();
         }
 
-        protected override void OnClosed(EventArgs e)
-        {
+        protected override void OnClosed(EventArgs e) {
             _source.RemoveHook(HwndHook);
             _source = null;
             UnregisterHotKey();
@@ -180,31 +191,25 @@ namespace CodeSwine_Solo_Public_Lobby
             base.OnClosed(e);
         }
 
-        private void RegisterHotKey()
-        {
+        private void RegisterHotKey() {
             var helper = new WindowInteropHelper(this);
-            const uint VK_F10 = 0x79;
+            const uint VK_F12 = 0x7B;
             const uint MOD_CTRL = 0x0002;
-            if (!RegisterHotKey(helper.Handle, HOTKEY_ID, MOD_CTRL, VK_F10))
-            {
-                
+            if (!RegisterHotKey(helper.Handle, HOTKEY_ID, MOD_CTRL, VK_F12)) {
+
             }
         }
 
-        private void UnregisterHotKey()
-        {
+        private void UnregisterHotKey() {
             var helper = new WindowInteropHelper(this);
             UnregisterHotKey(helper.Handle, HOTKEY_ID);
         }
 
-        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
-        {
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled) {
             const int WM_HOTKEY = 0x0312;
-            switch (msg)
-            {
+            switch (msg) {
                 case WM_HOTKEY:
-                    switch (wParam.ToInt32())
-                    {
+                    switch (wParam.ToInt32()) {
                         case HOTKEY_ID:
                             OnHotKeyPressed();
                             handled = true;
@@ -215,9 +220,8 @@ namespace CodeSwine_Solo_Public_Lobby
             return IntPtr.Zero;
         }
 
-        private void OnHotKeyPressed()
-        {
-            SetRules();
+        private void OnHotKeyPressed() {
+            ToggleInternet();
             System.Media.SystemSounds.Hand.Play();
         }
     }
